ed8244a60b6e02541efb295cbd6aa759c9cb94de
rohith <101670317+Rohithgg@users.noreply.github.com>
2024-11-17 13:14:28 +0000

    modified:   member.js

--- member.js
+++ member.js
@@ -2,8 +2,8 @@ function skillsMemeber(){
     return {
         restrict: 'E',
         templateUrl: 'app/views/skills.html',
-        controller: 'skillsController',
-        controllerAs: 'skillsCtrl',
+        controller: 'skillMemberController',
+        controllerAs: 'vm',
         bindToController: true,
         scope: {
             skills: '='
